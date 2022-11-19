e77f4db7d53e6f38dcd8fb90e88609b89d21edc7
yusufadell <youseefadel777@gmail.com>
2022-11-19 13:13:09 +0200

    setup: add dynamic package versionning

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+from pyqa import __version__
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
@@ -48,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/yusufadell/pyqa",
-    version="0.1.0",
+    version=__version__,
     zip_safe=False,
 )
