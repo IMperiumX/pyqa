3d67829b8f7cd00bc6ad3c92eb4ef93e412a2814
yusufadell <youseefadel777@gmail.com>
2022-11-19 13:17:38 +0200

    cli(query): get answer from query and display in rich panel

--- pyqa/cli.py
+++ pyqa/cli.py
@@ -1,61 +1,74 @@
 """Console script for pyqa."""
 import sys
+
 import click
-import logging
-import os
+from rich import print
+from rich.align import Align
+from rich.panel import Panel
+from rich.text import Text
 
-"""
-"-f", "--foo-bar", the name is foo_bar
+from pyqa import __version__
+from pyqa import utils
+from pyqa.constants import LOGO, URL, DEFUALT_QUERY, EPILOG
 
-"-x", the name is x
 
+@click.group()  # invoke_without_command=True
+@click.version_option(message="pyQA, version %(version)s")
+@click.option("-a", "--all", is_flag=True)
 "-f", "--filename", "dest", the name is dest
+@click.option(
+    "-e",
+    "--engine",
+    help="search engine for this query (google, bing, duckduckgo)",
+    default="bing",
+)
+@click.pass_context
+def main(ctx, **kwargs):
+    ctx.ensure_object(dict)
+    args = ctx.obj
 
-"--CamelCase", the name is camelcase
+    query = kwargs.get("query", "yusufadell")
+    args["engine"] = kwargs["engine"]
+    args["all"] = kwargs["all"]
 
 "-f", "-fb", the name is f
 
 "--f", "--foo-bar", the name is f
 
-"---f", the name is _f
-"""
-from pyqa import __version__
-
-
-@click.group(invoke_without_command=True)
-@click.version_option(message="pyQA, version %(version)s")
+@main.command(epilog=EPILOG)
 @click.argument("query", nargs=-1)
 @click.pass_context
-def main(ctx, query):
-    ctx.ensure_object(dict)
+def query(ctx, **kwargs):
     args = ctx.obj
-    args["runner"] = "pyQA"
-    query = " ".join(query)
-    args["query"] = query
-    args["search_engine"] = "google"
-    click.echo(args)
+    query = kwargs["query"] or DEFUALT_QUERY
+    args["query"] = " ".join(query)
 
-
-@click.option(
-    "--version",
-    expose_value=False,
-    is_flag=True,
+    print(
+        Panel(
+            Align.center(
+                Text.from_ansi(LOGO, no_wrap=True),
+                vertical="middle",
+            ),
+            border_style="green",
+            title="pyQA",
+            subtitle="Thank you for using pyQA",
 )
-def version():
-    click.echo(f"pyQA, version {__version__}")
+    )
 
-
-@main.command()
-@click.option(
-    "--explain",
-    help="Print explanation of the error.",
-    expose_value=False,
-    show_default=True,
+    url = URL  # TODO: get link from query
+    res = utils._get_answer(args, url)
+    answer = res
+    print(
+        Panel(
+            Align.center(
+                Text.from_ansi(answer, no_wrap=True),
+                vertical="middle",
+            ),
+            border_style="green",
+            title="pyQA",
+            subtitle="Tank you for using pyQA",
 )
-def explain():
-    """Explain the error."""
-    logging.getLogger().setLevel(logging.INFO)
-    logging.info("Version: %s", __version__)
+    )
 
 
 @main.command()
