6cf8a06010e2b43c1534f11cb58d326ed80d598e
Yusuf Adel <yousefadel@paymob.com>
2023-09-07 20:55:44 +0300

    Add GitHub username to package metadata

--- pyqa/__init__.py
+++ pyqa/__init__.py
@@ -3,9 +3,14 @@
 __author__ = """Yusuf Adel"""
 __email__ = "yusufadell.dev@gmail.com"
 __version__ = "0.3.8"
+__github_username__ = "ImperiumX"
 
 from pyqa.pyqa import command_line_runner
 
 __all__ = [
+    "__author__",
+    "__email__",
+    "__version__",
+    "__github_username__",
     "command_line_runner",
 ]
